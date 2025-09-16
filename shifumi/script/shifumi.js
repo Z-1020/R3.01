0092be28d45abcb9537cc341d51896b709dba2f5
Z-1020 <zoemargerie14@gmail.com>
2025-09-16 17:55:35 +0200

    affichage

--- shifumi/script/shifumi.js
+++ shifumi/script/shifumi.js
@@ -1,12 +1,30 @@
 const bouton = document.querySelectorAll('.bouton')
 const texte = document.getElementById('choix_utilisateur')
-texte.textContent = bouton.value;
+const texte2 = document.getElementById('choix-robot')
+const texte3 = document.getElementById('resultat')
 
+let nbVictoire =0;
+let nbEgalite =0;
+let nbDefaite =0;
+const choix = ["Feuille", "Pierre", "Ciseaux"]
+let choixOrdi;
 bouton.forEach(bouton => {
     bouton.addEventListener("click", ()=>{
         if(bouton.value === "Feuille" || bouton.value === "Pierre" || bouton.value === "Ciseaux"){
             texte.textContent = bouton.value;
         }
+        choixOrdi = choix[Math.floor(Math.random() * 3)]
+
+           if(choixOrdi === 0){
+                texte2.textContent = choixOrdi
+            }
+            else if(choixOrdi  === 1){
+                texte2.textContent = choixOrdi
+            }
+            else if(choixOrdi === 2){
+                texte2.textContent = choixOrdi
+            }
+
         
     })
 });
