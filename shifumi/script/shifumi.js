bfca3955d8d76db3a5ec9007a44467cb671934f9
Z-1020 <zoemargerie14@gmail.com>
2025-09-18 22:40:28 +0200

    version finale

--- shifumi/script/shifumi.js
+++ shifumi/script/shifumi.js
@@ -6,7 +6,7 @@ const victoire = document.getElementById('victoire')
 const defaite = document.getElementById('defaite')
 const egalite = document.getElementById('egalite')
 const reset = document.querySelector('.bouton-reinitialiser')
-const resultat = document.querySelector('.resultat')
+const resultat = document.getElementById('resultat')
 
 let nbVictoire =0
 let nbEgalite =0
@@ -25,19 +25,19 @@ bouton.forEach(bouton => {
             texte3.textContent = "égalité"
             nbEgalite ++
             egalite.textContent ="Nombre d'égalité: "+ nbEgalite
-            resultat.setAttribute('class', '"alert alert-success"')
+            resultat.className = 'alert alert-warning display-6 text-center'
         }
         else if((bouton.value === "Pierre" && choixOrdi === "Ciseaux") || (bouton.value === "Feuille" && choixOrdi === "Pierre") || (bouton.value === "Ciseaux" && choixOrdi === "Feuille") ){
             texte3.textContent = "Vous avez gagné"
             nbVictoire ++
             victoire.textContent = "Nombre de Victoire: "+ nbVictoire
-            resultat.setAttribute('class', '"alert alert-success"')
+            resultat.className = 'alert alert-success display-6 text-center'
         }
         else if((bouton.value === "Ciseaux" && choixOrdi === "Pierre") || (bouton.value === "Pierre" && choixOrdi === "Feuille") || (bouton.value === "Feuille" && choixOrdi === "Ciseaux") ){
             texte3.textContent = "Vous avez perdu"
             nbDefaite ++
             defaite.textContent = "Nombre de défaite: " + nbDefaite
-            resultat.setAttribute('class', '"alert alert-success"')
+            resultat.className = 'alert alert-danger display-6 text-center'
         }
 
         
@@ -49,6 +49,8 @@ bouton.forEach(bouton => {
         victoire.textContent = "Nombre de Victoire: "+ nbVictoire
         defaite.textContent = "Nombre de défaite: " + nbDefaite
         egalite.textContent ="Nombre d'égalité: "+ nbEgalite
+        resultat.className = "display-6 text-center"
+        texte3.textContent = "Résultat"
     })
 
 });
