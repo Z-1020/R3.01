f6ca70cf254ddc00a876f38f56869747145246cb
Z-1020 <zoemargerie14@gmail.com>
2025-09-20 22:11:28 +0200

    feat: add exercice 1

---
+++ Exercice_js/Exercice1.js
@@ -0,0 +1,11 @@
+const Personne ={
+     prenom : "Jean",
+     nom: "Jacque",
+     age: 24,
+     ville: "Strasbourg"
+}
+
+console.log("Je m'appelle "+Personne.prenom)
+console.log("mon nom est "+Personne.nom)
+console.log("J'ai "+Personne.age + " ans")
+console.log("J'habite à "+Personne.ville)
