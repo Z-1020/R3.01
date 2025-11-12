9beba27dd3d40316990078de448c5ccf37cc8ab5
Z-1020 <zoemargerie14@gmail.com>
2025-11-12 08:27:44 +0100

    add: ajout de la base de donnée

---
+++ bdd.sql
@@ -0,0 +1,2 @@
+CREATE TABLE `minichat`.`conversation` (`pseudo` VARCHAR NOT NULL , `message` INT NOT NULL , `date` DATE NOT NULL , `heure` DATETIME NOT NULL , `num` INT NOT NULL ) ENGINE = InnoDB;
+ALTER TABLE `conversation` ADD PRIMARY KEY(`num`);
