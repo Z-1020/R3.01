8a9bb690eda3d343ee53a318b87cf492a498df6f
Z-1020 <zoemargerie14@gmail.com>
2025-11-16 00:15:12 +0100

    add: ajout style du tchat

--- index.php
+++ index.php
@@ -6,35 +6,41 @@
     ?>
     <head>
         <title>Minichat</title>
+        <link href="Style/style.css" rel="stylesheet">
     </head>
     <body>
-        <form action="traitementMinichat.php" method="post">
-            <label for="pseudo"> Veuillez entrer votre pseudo: </label>
-            <input type="texte" id="pseudo" name="pseudo"/>
-            <br>
-            <label for="message"> Veuillez entrer votre message: </label>
-            <input type="texte" id="message" name="message"/>
-            <br>
-            <input type="submit" id="envoyer" value="Envoyer"/>
-
-
-        </form>
-        <hr>
-        <?php
-            $resMessage = [];
-           $sth = $conn->prepare("select pseudo, message, date from conversation order by date desc limit 5 ");
-           $sth->execute();
-           $res = $sth->fetchAll(PDO::FETCH_ASSOC);
-          foreach($res as $key => $val){
-            echo $val['date']. ' '.$val['pseudo']. ' à envoyé : '. $val['message'];
-            echo '</br>';
-          }
-        
-           
-        ?>
+        <header>
+            <h1>Bienvenue sur le tchat </h1>
+        </heaser>
+        <main>
+            <div class="formulaire">
+                <form action="traitementMinichat.php" method="post">
+                    <label for="pseudo"> Veuillez entrer votre pseudo: </label>
+                    <input type="texte" id="pseudo" name="pseudo" placeholder="pseudo"/>
+                    </br>
+                    <label for="message"> Veuillez entrer votre message: </label>
+                    <input type="texte" id="message" name="message" placeholder="message"/>
+                    </br>
+                    <input type="submit" id="envoyer" value="Envoyer"/>
+                </form>
+            </div>
+            <hr>
+            <div class="message">
+            <?php
+                $resMessage = [];
+                $sth = $conn->prepare("select pseudo, message, date from conversation order by date desc limit 5 ");
+                $sth->execute();
+                $res = $sth->fetchAll(PDO::FETCH_ASSOC);
+                foreach($res as $key => $val){
+                    echo $val['date']. ' '.$val['pseudo']. ' à envoyé : '. $val['message'];
+                    echo '</br>';
+                }  
+            ?>
+            </div>
+        </main>
+        <footer>
+        </footer>
     </body>
-    
-
 </html>
 
 
